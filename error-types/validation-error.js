01a4e090309833dea423aaf95af99caccf9102e8
Srivatsan <vatsan1993@gmail.com>
2023-09-04 17:09:04 +0530

    error in tests of validation-error

--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -1,5 +1,15 @@
 // Your code here
-
+class ValidationError extends Error {
+  constructor(message = 'Invalid input', ...params) {
+    super(...params);
+    // Maintains proper stack trace for where error was thrown (available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationError);
+    }
+    this.name = 'ValidationError';
+    this.message = message;
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
