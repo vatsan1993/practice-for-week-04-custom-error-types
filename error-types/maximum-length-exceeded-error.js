05fd2a7e257515eedfbd4ad6103cf1521e02563c
Srivatsan <vatsan1993@gmail.com>
2023-09-04 17:46:53 +0530

    coded max length exceeded error

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -1,7 +1,21 @@
 const ValidationError = require('./validation-error');
 
 // Your code here
-
+class MaximumLengthExceededError extends ValidationError {
+  constructor(excessLength = null, ...params) {
+    super(...params);
+    // Maintains proper stack trace for where error was thrown (available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, MaximumLengthExceededError);
+    }
+    this.name = 'MaximumLengthExceededError';
+    if (excessLength == null) {
+      this.message = `Maximum length exceeded`;
+    } else {
+      this.message = `Maximum length exceeded by ${excessLength}`;
+    }
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
