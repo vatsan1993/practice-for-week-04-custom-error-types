c3219019d9c7e5f4f778e637d729d5b8fe3a7e96
Srivatsan <vatsan1993@gmail.com>
2023-09-08 19:39:25 +0530

    fixed the errors

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -5,9 +5,9 @@ class MaximumLengthExceededError extends ValidationError {
   constructor(excessLength = null, ...params) {
     super(...params);
     // Maintains proper stack trace for where error was thrown (available on V8)
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, MaximumLengthExceededError);
-    }
+    // if (Error.captureStackTrace) {
+    //   Error.captureStackTrace(this, MaximumLengthExceededError);
+    // }
     this.name = 'MaximumLengthExceededError';
     if (excessLength == null) {
       this.message = `Maximum length exceeded`;
